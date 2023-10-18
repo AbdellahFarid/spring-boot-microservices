63bdcce3783529935f03328df776529c7d5ee75f
Mon Ordi <abdellah.farid@etu.uae.ac.ma>
2023-10-18 18:36:17 +0100

    finished adding mapping libraries(modelmapper and mapstruct)

--- employee-service/employee-service/src/main/java/com/afarid/employeeservice/mapper/EmployeeMapper.java
+++ employee-service/employee-service/src/main/java/com/afarid/employeeservice/mapper/EmployeeMapper.java
@@ -1,2 +1,16 @@
-package com.afarid.employeeservice.mapper;public class EmployeeMapper {
+package com.afarid.employeeservice.mapper;
+
+import com.afarid.employeeservice.dto.EmployeeDto;
+import com.afarid.employeeservice.entity.Employee;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface EmployeeMapper {
+
+    EmployeeMapper EMPLOYEE_MAPPER = Mappers.getMapper(EmployeeMapper.class);
+
+    EmployeeDto toEmployeeDto(Employee employee);
+
+    Employee toEmployee(EmployeeDto employeeDto);
 }
--- employee-service/employee-service/src/main/java/com/afarid/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/employee-service/src/main/java/com/afarid/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -2,22 +2,23 @@ package com.afarid.employeeservice.service.impl;
 
 import com.afarid.employeeservice.dto.EmployeeDto;
 import com.afarid.employeeservice.entity.Employee;
+import com.afarid.employeeservice.mapper.EmployeeMapper;
+import com.afarid.employeeservice.mapper.EmployeeModelMapper;
 import com.afarid.employeeservice.repository.EmployeeRepository;
 import com.afarid.employeeservice.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
     private final EmployeeRepository employeeRepository;
+    private final EmployeeModelMapper employeeModelMapper;
     @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
 
         //Using constructor
-        Employee employee = new Employee(
+/*        Employee employee = new Employee(
                 employeeDto.getId(),
                 employeeDto.getFirstName(),
                 employeeDto.getLastName(),
@@ -30,13 +31,26 @@ public class EmployeeServiceImpl implements EmployeeService {
                 savedEmployee.getFirstName(),
                 savedEmployee.getLastName(),
                 savedEmployee.getEmail()
-        );
+        );*/
+        /*//Using MapStruct
+        Employee employee = EmployeeMapper.EMPLOYEE_MAPPER.toEmployee(employeeDto);
+        //Save the employee in the database
+        Employee savedEmployee = employeeRepository.save(employee);
+
+        return EmployeeMapper.EMPLOYEE_MAPPER.toEmployeeDto(savedEmployee);*/
+
+        //Using ModelMapper
+        Employee employee = employeeModelMapper.mapToEmployee(employeeDto);
+        //Save the employee in the database
+        Employee savedEmployee = employeeRepository.save(employee);
+
+        return employeeModelMapper.mapToEmployeeDto(savedEmployee);
     }
 
     @Override
     public EmployeeDto getEmployeeById(Long id) {
 
-        //Using the constructor
+/*        //Using the constructor
         Employee employee = employeeRepository.findById(id).get();
 
         return new EmployeeDto(
@@ -44,6 +58,14 @@ public class EmployeeServiceImpl implements EmployeeService {
                 employee.getFirstName(),
                 employee.getLastName(),
                 employee.getEmail()
-        );
+        );*/
+
+/*        //Using MapStruct
+        Employee employee = employeeRepository.findById(id).get();
+        return EmployeeMapper.EMPLOYEE_MAPPER.toEmployeeDto(employee);*/
+
+        Employee employee = employeeRepository.findById(id).get();
+
+        return employeeModelMapper.mapToEmployeeDto(employee);
     }
 }

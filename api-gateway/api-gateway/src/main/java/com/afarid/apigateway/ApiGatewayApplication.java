fc57c3e3b39291f4bc79ead56455ea5c9890a48b
Mon Ordi <abdellah.farid@etu.uae.ac.ma>
2023-11-29 17:06:32 +0100

    initial commit for gateway service

---
+++ api-gateway/api-gateway/src/main/java/com/afarid/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.afarid.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.cloud.gateway.route.RouteDefinitionRouteLocator;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
